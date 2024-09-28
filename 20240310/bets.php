a4b23677d695ec0e782560abe748d2f4afd65940
adoula4141 <elbourichi.adil@gmail.com>
2024-09-28 05:49:13 +0000

    ...

---
+++ 20240310/bets.php
@@ -0,0 +1,233 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '1, 6, 11',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'official win' => '11, 6, 5, 14',
+		'win amount' => 63,
+		'qin amount' => 142,
+		'trio amount' => 435,
+		'place amount' => [
+			11 => 19,
+			6 => 19.5,
+			5 => 40.5,
+		],
+		'win inter' => '11',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2, 4, 5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'official win' => '5, 10, 14, 2',
+		'win amount' => 50.5,
+		'qin amount' => 812,
+		'trio amount' => 2998,
+		'place amount' => [
+			5 => 18,
+			10 => 72,
+			14 => 31,
+		],
+		'win inter' => '4, 7, 9, 10',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+		'bets' => [
+			'place(end-favorites , $100)' => '5',
+			'1 won(place bet)' => 180,
+			'place(end-wp , $100)' => '5',
+			'2 won(place bet)' => 180,
+		],
+		'total bets' => 200,
+		'total won in race 2' => 160,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '2, 3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'official win' => '10, 3, 4, 5',
+		'win amount' => 61.5,
+		'qin amount' => 58.5,
+		'trio amount' => 215,
+		'place amount' => [
+			10 => 18,
+			3 => 11,
+			4 => 32,
+		],
+		'win inter' => '2, 3, 7, 9, 11, 12',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'official win' => '5, 3, 6, 11',
+		'win amount' => 23.5,
+		'qin amount' => 342,
+		'trio amount' => 415,
+		'place amount' => [
+			5 => 13,
+			3 => 50,
+			6 => 20.5,
+		],
+		'win inter' => '1, 2, 4, 5, 6',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '1',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'official win' => '1, 8, 2, 7',
+		'win amount' => 21,
+		'qin amount' => 70,
+		'trio amount' => 291,
+		'place amount' => [
+			1 => 13,
+			8 => 17,
+			2 => 36,
+		],
+		'win inter' => '1, 2, 3, 4, 5, 7, 12',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 10, 11',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '3, 7',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14',
+		'official win' => '7, 2, 3, 4',
+		'win amount' => 20,
+		'qin amount' => 85,
+		'trio amount' => 76,
+		'place amount' => [
+			7 => 11,
+			2 => 22,
+			3 => 15.5,
+		],
+		'win inter' => '1, 3, 5, 8, 9',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '1, 3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'official win' => '2, 10, 9, 3',
+		'win amount' => 110,
+		'qin amount' => 572.5,
+		'trio amount' => 3213,
+		'place amount' => [
+			2 => 28.5,
+			10 => 26,
+			9 => 41,
+		],
+		'win inter' => '1, 2, 3, 5, 6, 10',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '4, 6, 8',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'official win' => '6, 8, 4, 7',
+		'win amount' => 65.5,
+		'qin amount' => 159,
+		'trio amount' => 125,
+		'place amount' => [
+			6 => 20.5,
+			8 => 20,
+			4 => 16,
+		],
+		'win inter' => '4, 5, 6, 8, 12',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12',
+		'bets' => [
+			'place(end-favorites , $100)' => '8',
+			'1 won(place bet)' => 200,
+			'super sure bet' => 'super sure place 8',
+			'5 won(place bet)' => 200,
+			'place(end-wp , $100)' => '8',
+			'2 won(place bet)' => 200,
+		],
+		'total bets' => 300,
+		'total won in race 8' => 300,
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '2, 3, 5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'official win' => '3, 5, 2, 14',
+		'win amount' => 77,
+		'qin amount' => 733,
+		'trio amount' => 859,
+		'place amount' => [
+			3 => 26.5,
+			5 => 47,
+			2 => 23.5,
+		],
+		'win inter' => '1, 4, 5, 8, 9, 10, 12',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+		'bets' => [
+			'place(end-wp , $100)' => '5',
+			'2 won(place bet)' => 470,
+		],
+		'total bets' => 100,
+		'total won in race 9' => 370,
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '3, 5, 11',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'official win' => '12, 3, 11, 5',
+		'win amount' => 92,
+		'qin amount' => 478,
+		'trio amount' => 676,
+		'place amount' => [
+			12 => 26.5,
+			3 => 30.5,
+			11 => 19,
+		],
+		'win inter' => '1',
+		'allValues' => '',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+];
+//total major place favorites: 180
+//total major place wp: 550
+//total sure place: 100
+//total: 830
