e150e7421c37c4df9e6189259e98bf84ecb5be25
adoula4141 <59900990+adoula4141@users.noreply.github.com>
2024-10-01 15:56:01 +0800

    Update bets.php

--- bets.php
+++ bets.php
@@ -82,11 +82,12 @@ for ($raceNumber = 1; $raceNumber <= $numberOfRaces; $raceNumber++) {
     if(isset($officialWin)){
         $racetext .= "\t\t'official win' => '" . implode(", ", $officialWin) . "',\n"; 
     }
-    $diff = array_diff($runners, $suggestions["trio"]);
-    if(!empty($diff)) $racetext .= "\t\t\t'diff' => '" . implode(", ", $diff) . "',//count diff: " . count($diff) . "\n";
-    $inter = array_intersect($favorites, $diff);
-    $racetext .= "\t\t\t'inter' => '" . implode(", ", $inter) . "',//count inter: " . count($inter) . "\n";
-    
+    if(!empty($suggestions["trio"])){
+        $diff = array_diff($runners, $suggestions["trio"]);
+        if(!empty($diff)) $racetext .= "\t\t\t'diff' => '" . implode(", ", $diff) . "',//count diff: " . count($diff) . "\n";
+        $inter = array_intersect($favorites, $diff);
+        $racetext .= "\t\t\t'inter' => '" . implode(", ", $inter) . "',//count inter: " . count($inter) . "\n";
+    }
     $racetext .= "\t\t'suggestions' => [\n";
     $racetext .= "\t\t\t'win' => '" . implode(", ", array_intersect($runners, $suggestions["win"])) . "',\n";
     $racetext .= "\t\t\t'qin' => '" . implode(", ", array_intersect($runners, $suggestions["qin"])) . "',\n";
